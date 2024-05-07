8f2573611cc8b6d0f45914a4a0f199baa2b20251
fangdown <409951398@qq.com>
2024-05-07 22:10:02 +0800

    docker

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const Koa = require('koa')
-const Router = require('koa-router')
-const mockList = require('./mock/index')
+const Koa = require("koa");
+const Router = require("koa-router");
+const cors = require("koa2-cors");
+const mockList = require("./mock/index");
 
-const app = new Koa()
-const router = new Router()
+const app = new Koa();
+const router = new Router();
 
 async function getRes(fn, ctx) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            const res = fn(ctx)
-            resolve(res)
-        }, 1000)
-    })
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const res = fn(ctx);
+      resolve(res);
+    }, 1000);
+  });
 }
 
 // 注册 mock 路由
-mockList.forEach(item => {
-    const { url, method, response } = item
-    router[method](url, async ctx => {
-        // const res = response()
-        const res = await getRes(response, ctx) // 模拟网络请求的加载状态，1s
-        ctx.body = res // 输入结果
-    })
-})
+mockList.forEach((item) => {
+  const { url, method, response } = item;
+  router[method](url, async (ctx) => {
+    // const res = response()
+    const res = await getRes(response, ctx); // 模拟网络请求的加载状态，1s
+    ctx.body = res; // 输入结果
+  });
+});
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(router.routes());
 
-app.use(router.routes())
-app.listen(11011) // port 端口
+app.listen(11011); // port 端口
